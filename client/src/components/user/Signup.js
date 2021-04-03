88a2cdff0189a055fcbf61a53d35d9faf57643b5
notGonim <mhmodgonim96@gmail.com>
2021-04-03 15:10:28 +0200

    ADDED : style for the register form

--- client/src/components/user/Signup.js
+++ client/src/components/user/Signup.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Signup = () => {
     return (
-        <div>
-            <h1>SignUP</h1>
-        </div>
+        <>
+            <div className="login_container">
+                <h1 >LogIn</h1>
+                <div className="form_container">
+                    <input type="text" placeholder="Write Your UserName " />
+                    <input type="email" placeholder="Write Your Email " />
+                    <input type="password" placeholder="Write Your Password" />
+                    <button className="btn_sign"> SignUp</button>
+                </div>
+                <p>if you already having an account <Link to='/login' className="span_color">Login</Link></p>
+            </div>
+        </>
     )
 }
