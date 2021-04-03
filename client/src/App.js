4e0c4978871fee8afd22de085c10b76da5511096
notGonim <mhmodgonim96@gmail.com>
2021-04-03 13:01:36 +0200

    CONFIG : routes for the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Home } from "./components/Home";
+import { Login } from "./components/user/Login";
+import { Signup } from "./components/user/Signup";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/signup' exact component={Signup} />
+        <Route path='/login' exact component={Login} />
       </Switch>
     </Router>
   );
