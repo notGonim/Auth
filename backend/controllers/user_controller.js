58553821408edfd4adb67339c8517fc977d6aafb
notGonim <mhmodgonim96@gmail.com>
2021-04-04 17:37:33 +0200

    ADDED : user_controller file and added the register func

---
+++ backend/controllers/user_controller.js
@@ -0,0 +1,17 @@
+import { asyncError } from "../middleware/catchAsyncErrors.js"
+import User from "../models/user_model.js"
+
+
+
+//register user   ===>   /api/signup 
+export const registerUser = asyncError(async (req, res, next) => {
+
+    const { name, email, password } = req.body
+    const user = await User.create({
+        name, email, password
+    })
+    res.status(201).json({
+        success: true,
+        user
+    })
+})
