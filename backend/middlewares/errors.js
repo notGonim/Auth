b9408ae53551e59ebb63c4100f99b196f1d9ac6f
notGonim <mhmodgonim96@gmail.com>
2021-04-04 13:57:14 +0200

    ADDED : the error file to handle the errors

---
+++ backend/middlewares/errors.js
@@ -0,0 +1,27 @@
+export const errors = (err, req, res, next) => {
+
+
+    err.statusCode = err.statusCode || 500
+
+    if (process.env.NODE_ENV === "DEVELOPMENT") {
+        res.status(err.statusCode).json(
+            {
+                success: false,
+                error: err,
+                errMessage: err.message,
+                stack: err.stack
+            }
+        )
+    }
+
+
+
+
+    res.status(err.statusCode).json(
+        {
+            success: false,
+            error: err.stack
+        }
+    )
+
+}
