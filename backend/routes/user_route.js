6a7cf1c65e3f9d09a045bc51ac45706e1b61bb5a
notGonim <mhmodgonim96@gmail.com>
2021-04-06 10:15:14 +0200

    ADDED : route for the user to login n

--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { registerUser } from '../controllers/user_controller.js'
+import { registerUser, loginUser } from '../controllers/user_controller.js'
 
 
 export const UserRouter = express.Router()
 
-
+//route to sign up user 
 UserRouter.post('/signup', registerUser)
+//route to login user 
+UserRouter.post('/login', loginUser)
