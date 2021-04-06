1293f73b2ffc17061ddbfc4f6706ccfb757315bf
notGonim <mhmodgonim96@gmail.com>
2021-04-06 11:44:08 +0200

    ADDED : logout func to enable user to log out

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -42,3 +42,17 @@ export const loginUser = asyncError(async (req, res, next) => {
     //sending token 
     sendToken(user, 200, res)
 })
+
+
+//Logout user   ===> /api/logout 
+export const logOut = asyncError(async (req, res, next) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: 'User Logged Out'
+    })
+})
