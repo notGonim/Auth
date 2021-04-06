53458052cbbb619af8c7fb35f3ce70fcff6fd083
notGonim <mhmodgonim96@gmail.com>
2021-04-06 16:54:17 +0200

    ADDED : passwordReset func to add the functionality of restting the password

--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser, loginUser, logOut, resetPassword } from '../controllers/user_controller.js'
+import { registerUser, loginUser, logOut, resetPassword, passwordReset } from '../controllers/user_controller.js'
 
 
 export const UserRouter = express.Router()
@@ -8,3 +8,4 @@ UserRouter.post('/signup', registerUser)
 UserRouter.post('/login', loginUser)
 UserRouter.get('/logout', logOut)
 UserRouter.post('/password/forgot', resetPassword)
+UserRouter.put('/password/reset/:token', passwordReset)
